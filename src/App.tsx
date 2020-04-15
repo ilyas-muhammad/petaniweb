0a46b9a43c0c287f2e0209c77aaa240cc612ed7b
Ilyas D Jayanto <ell@petaniweb.com>
2020-04-15 19:58:13 +0700

    structuring project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
+import ProTip from './component/ProTip';
 
 function Copyright() {
   return (
--- src/component/ProTip.tsx
+++ src/component/ProTip.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
 import SvgIcon, { SvgIconProps } from '@material-ui/core/SvgIcon';
 import Typography from '@material-ui/core/Typography';
 
