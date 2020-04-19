a51660b35da27a339f11f4513e34ff8672d788db
Nadiah Nahdah <nadiah.nahdah@ioc.sc>
2020-04-19 19:08:09 +0700

    add styled-components

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,7 +4,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 // A custom theme for this app
 const theme = createMuiTheme({
   typography: {
-    fontFamily: 'poppins',
+    fontFamily: 'Poppins',
   },
   palette: {
     background: {
