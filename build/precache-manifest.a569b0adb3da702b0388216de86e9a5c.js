e5f9a4daeb0899097d802d82ad5af987fc7cd5a4
Ilyas D Jayanto <ell@petaniweb.com>
2020-04-23 01:07:50 +0700

    try not ignore build

---
+++ build/precache-manifest.a569b0adb3da702b0388216de86e9a5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3572917fae161f95bc19a58c164ca06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844de6ddabeb4e3a90a3",
+    "url": "/static/js/2.60c104a3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.60c104a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a63df932712186b16f7",
+    "url": "/static/js/main.70671cc0.chunk.js"
+  },
+  {
+    "revision": "4e40c02570a94ca7b912",
+    "url": "/static/js/runtime-main.0b2a8f17.js"
+  }
+]);
